1d95a971b446d29d8964770480ab1ba21c1d4a94
jjunyuongv <jjunyuongv@gmail.com>
2025-05-22 12:24:56 +0900

    도망가지마요

---
+++ react04-board2-with-router/보관/src-03모듈화/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import { Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom'
+import List from './components/board/List';
+import NotFound from './components/common/NotFound';
+import Write from './components/board/Write';
+import View from './components/board/View';
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<List></List>} />
+          <Route path='/list' element={<List></List>} />
+          <Route path='/view' element={<View></View>} />
+          <Route path='/write' element={<Write></Write>} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App
