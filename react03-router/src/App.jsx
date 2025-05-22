1c55c6771df546ff1f451805590f8836fc8bb525
jjunyuongv <jjunyuongv@gmail.com>
2025-05-22 09:57:35 +0900

    Hi

--- react03-router/src/App.jsx
+++ react03-router/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Route, Routes } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
 
 const TopNavi = ()=> {
   return (
@@ -14,17 +15,73 @@ const TopNavi = ()=> {
 
 const CommonLayout = () => {
   return (
-
+    <div>
+      <header style={{ background:'lightgray', padding:'10px'}}>
+        Outlet 컴포넌트 알아보기
+      </header>
+      <article>
+          {/* 각 페이지의 컴포넌트가 보여지는 부분에 설정한다. */}
+          <Outlet />
+      </article>
+      <footer style={{ background:'lightgray', padding:'10px' }}>
+        공통 레이아웃
+      </footer>
+    </div>
   );
 };
+
+const Home = () => {
+  return (<>
+    <h2>React Home</h2>
+    <p>
+      React Router에 대해 학습합니다.
+    </p>
+  </>);
+}
+
+const LayoutIndex = () => {
+  return (<>
+    <h2>레이아웃 인덱스 페이지</h2>
+    <ul>
+      <li>Outlet 컴포넌트 위치에 출력됩니다.</li>
+      <li>Route 설정시 index로 지정합니다.</li>
+    </ul>
+  </>);
+}
+
+const RouterHooks = () => {
+  return (<>
+    <h2>라우터 관련 Hook</h2>
+  </>);
+}
+
+const NotFound = () =>{
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>
+        페이지를 찾을 수 없습니다. ㅜㅜ <br />
+        <Link to='/'>Home</Link> 
+      </p>
+    </div>
+  );
+}
 
 function App() {
 
   return (
     <div className='App'>
       <TopNavi></TopNavi>
+      <Routes>  
+        <Route path='/' element={<Home></Home>} />
+        <Route path='/intro' element={<CommonLayout />}>
+          <Route index element={<LayoutIndex />} />
+          <Route path="router" element={<RouterHooks />} />
+        </Route>
+        <Route path='*' element={<NotFound></NotFound>} />
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App
