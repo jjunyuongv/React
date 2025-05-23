011f26cd7ecda490d12166af37e0159cb3607fb0
jjunyuongv <jjunyuongv@gmail.com>
2025-05-23 13:17:08 +0900

    도망금지

--- project01-comments/src/App.jsx
+++ project01-comments/src/App.jsx
@@ -42,7 +42,6 @@ function App() {
     setMyData(newMyData);
   }
 
-
   return (
     <div className="App">
       <Board />
--- project01-comments/src/commons/ComEdit.jsx
+++ project01-comments/src/commons/ComEdit.jsx
@@ -2,37 +2,53 @@ import React from "react";
 import {useState} from 'react';
 
 function ComEdit(props){
-  const [writer, setWriter] = useState(props.selectData.writer);
-  const [comment, setComment] = useState(props.selectData.comment);
+  const [writer, setWriter] = useState(props.writer);
+  const [comment, setComment] = useState(props.comment);
+  
   return (<>
     <form onSubmit={(event)=>{
       event.preventDefault();
       //폼값 가져오기
+      if (event.target.writer.value === '') {
+        alert('작성자를 입력하세요.');
+        event.target.writer.focus();
+        return;
+      }
+      if (event.target.writer.value === '') {
+        alert('댓글 내용을 입력하세요.');
+        event.target.comment.focus();
+        return;
+      }
+
+
       let writer = event.target.writer.value;
       let comment = event.target.comment.value;
-      //입력값 지우기
+      props.onEditComment(props.no, writer, comment);
       event.target.writer.value = '';
       event.target.comment.value = '';
       //수정 처리 하기
-      props.editAction(writer, comment);
+      props.setShowEdit(false);
+      props.setEditNo(null);
     }}>
       <table id="boardTable">
         <tbody>
           <tr>
             <td id="writer">Writer : 
-              <input type="text" name="writer" value={writer} onChange={(event)=>{
+              <input type="text" name="writer" onChange={(event)=>{
               setWriter(event.target.value);
-              }} />
-              <input type="button" value="수정취소" onClick={()=>{
-                props.changeMode('list', null);
-              }}></input>
+              }} value={writer} />
+             <button onClick={(event)=> {
+                event.preventDefault();
+                props.setShowEdit(false);
+                props.setEditNo(null);
+             }}>수정취소</button>
             </td>
             <td rowSpan="2"><input type="submit" value="댓글수정" id="btn"/></td>
           </tr>
           <tr>
-            <td><textarea name="comment" value={comment} onChange={(event)=>{
+            <td><textarea name="comment" onChange={(event)=>{
               setComment(event.target.value);
-            }}></textarea></td>
+            }} value={comment}></textarea></td>
           </tr>
         </tbody>
       </table>        
